8878d929e2b0b7666a13e1ddba7c85fd4b2337b1
vagrant <kennethracadio@hotmail.com>
2018-01-30 16:49:01 +0000

    Task 5: Length of input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $( document ).ready(function() {
     // console.log( "ready!" );
     $('.new-tweet').on('keyup', 'textarea', function(e) {
-      console.log('event: ', e);
-      console.log('key: ', e.key);
-      console.log(this);
+      // console.log('event: ', e);
+      console.log('length: ', this.value.length);
+      console.log('value: ', this.value);
+
     });
 
 });

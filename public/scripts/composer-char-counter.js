ec3a867b6e746f3653128e5eea23f3ecae8db166
vagrant <kennethracadio@hotmail.com>
2018-01-30 17:34:16 +0000

    Task 6: Target & Update the counter element

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,11 @@ $( document ).ready(function() {
     // console.log( "ready!" );
     $('.new-tweet').on('keyup', 'textarea', function(e) {
       // console.log('event: ', e);
-      console.log('length: ', this.value.length);
-      console.log('value: ', this.value);
-
+      console.log('length: ', this);
+      // console.log($(this));
+      let tweetLength = this.value.length;
+      console.log(tweetLength);
+      $(this).siblings('.counter').text(tweetLength);
     });
 
 });

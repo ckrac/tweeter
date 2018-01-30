5304bf5575b7bebe6bbcb72cb587d7538ee66113
vagrant <kennethracadio@hotmail.com>
2018-01-30 23:39:21 +0000

    Task 2: Implement renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,24 @@
 
 $( document ).ready(function() {
 
-  const tweetData = {
+  // const tweetData = {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": {
+  //       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+  //       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+  //       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  //     },
+  //     "handle": "@SirIsaac"
+  //   },
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // }
+
+  const data = [
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -20,12 +37,42 @@ $( document ).ready(function() {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
   }
+];
 
-  let $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweets').append($tweet);
-  $('#tweets').append($tweet);
+  // let $tweet = createTweetElement(tweetData);
+  // console.log($tweet);
+  // $('#tweets').append($tweet);
+  // $('#tweets').append($tweet);
 
   function createTweetElement (tweetObj) {
 
@@ -55,4 +102,17 @@ $( document ).ready(function() {
     return $tweet;
   }
 
+  function renderTweets(tweets) {
+  // loops through tweets
+    for (let tweet_ of tweets) {
+    // calls createTweetElement for each tweet
+    let $tweetValue =createTweetElement(tweet_);
+    // takes return value and appends it to the tweets container
+    $('#tweets').append($tweetValue);
+    }
+  }
+
+  renderTweets(data);
+
 });
+
